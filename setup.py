540c7f381c00b72d9ade4588cccd7f0d048529f1
MullaRohan <mdrohanislam444@gmail.com>
2026-05-15 08:16:48 +0600

    requirement and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="easy_visa",
+    version="0.0.0",
+    author="MullaRohan",
+    author_email="rohanmulla.cs@gmail.com",
+    packages=find_packages(),
+)
